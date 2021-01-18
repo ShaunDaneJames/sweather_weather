27ebaf49374f8f3d3e7290f09daa598146294faa
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-18 16:15:11 -0700

    Adds route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :forecast, only: %i[index]
+
+      resources :backgrounds, only: %i[index]
+
+      resources :users, only: %i[create]
+
+      resources :sessions, only: %i[create]
+    end
+  end
 end
