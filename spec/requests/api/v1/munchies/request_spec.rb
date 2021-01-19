81e4603b2f249baf39244b8a123165f189676441
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-19 11:57:26 -0700

    Adds more roubust test for final commit

--- spec/requests/api/v1/munchies/request_spec.rb
+++ spec/requests/api/v1/munchies/request_spec.rb
@@ -6,9 +6,15 @@ describe 'exposes GET /api/v1/munchies?start=denver,co&end=pueblo,co&food=chines
     get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
 
     expect(response).to be_successful
-    data = JSON.parse(response.body, symbolize_names: true)
-
-    expect(data).to have_key(:id)
-    expect(data[:id]).to be_an(Integer)
+    list = JSON.parse(response.body, symbolize_names: true)
+    expect(list[:data].first).to have_key(:id)
+    expect(list[:data].first).to have_key(:type)
+    expect(list[:data].first[:type]).to eq('munchie')
+    expect(list[:data].first).to have_key(:attributes)
+    expect(list[:data].first[:attributes]).to be_an(Hash)
+    expect(list[:data].first[:attributes]).to have_key(:destination_city)
+    expect(list[:data].first[:attributes][:destination_city]).to be_a(String)
+    expect(list[:data].first[:attributes]).to have_key(:travel_time)
+    expect(list[:data].first[:attributes][:travel_time]).to be_a(Float)
   end
 end
