37eb4737007065892be453cd34143d66e876c143
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-19 19:24:30 -0700

    Adds sad path for road trip

--- spec/requests/api/v1/road_trip/request_spec.rb
+++ spec/requests/api/v1/road_trip/request_spec.rb
@@ -26,4 +26,22 @@ describe 'exposes POST /api/v1/road_trip' do
     expect(road_trip[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
     expect(road_trip[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
   end
+
+  it 'renders an unauthorized message if presented with bad credentials' do
+    user = User.create!(email: 'someone@example.com',
+                        password: 'EZPZ123',
+                        password_digest: 'EZPZ123',
+                        api_key: 'b3083e4e-250f-490f-971f-f764f242fd71')
+    road_trip_data =
+    {
+      "origin": "Denver,CO",
+      "destination": "Pueblo,CO",
+      "api_key": ""
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    post '/api/v1/road_trip', headers: headers, params: JSON.generate(road_trip_data)
+
+    expect(response.status).to eq(401)
+  end
 end
